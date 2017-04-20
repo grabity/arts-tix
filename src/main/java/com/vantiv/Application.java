4a3776f249c52aabd246c388dd0f080da8d3c270
Gary T. Usleaman <gusleaman@users.noreply.github.com>
2017-04-20 11:47:55 -0400

    Facebook login

--- src/main/java/com/vantiv/Application.java
+++ src/main/java/com/vantiv/Application.java
@@ -1,15 +1,38 @@
 package com.vantiv;
 
 import java.util.Arrays;
+import java.security.Principal;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class Application {
+@EnableOAuth2Sso
+@RestController
+public class Application extends WebSecurityConfigurerAdapter {
+	
+	@RequestMapping("/user")
+	public Principal user(Principal principal) {
+		return principal;
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// @formatter:off
+		http.antMatcher("/**").authorizeRequests().antMatchers("/", "/login**", "/webjars/**").permitAll().anyRequest()
+				.authenticated().and().logout().logoutSuccessUrl("/").permitAll().and().csrf()
+				.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+		// @formatter:on
+	}
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
