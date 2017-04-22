9f769f8d384fa056085375c1786e32f6461be031
Gary T. Usleaman <gusleaman@gmail.com>
2017-04-22 19:14:25 -0400

    Resync

--- src/main/java/com/vantiv/BaseController.java
+++ src/main/java/com/vantiv/BaseController.java
@@ -1,15 +1,38 @@
 package com.vantiv;
 
 
+import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.facebook.api.Facebook;
+import org.springframework.social.facebook.api.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class BaseController {
 
-    @RequestMapping("/")
-    public String index() {
-        return "index";
+    private Facebook facebook;
+    private ConnectionRepository connectionRepository;
+
+    public BaseController(Facebook facebook, ConnectionRepository connectionRepository) {
+        this.facebook = facebook;
+        this.connectionRepository = connectionRepository;
     }
 
-}
+    @GetMapping
+    public String helloFacebook(Model model) {
+        if (connectionRepository.findPrimaryConnection(Facebook.class) == null) {
+            return "redirect:/connect/facebook";
+        }
+
+        String [] fields = { "id", "email",  "name" };
+        User userProfile = facebook.fetchObject("me", User.class, fields);
+        
+        model.addAttribute("userName", userProfile.getName());
+        model.addAttribute("userId", userProfile.getId());
+        return "index";
+    }
+    
+ }
--- src/main/java/com/vantiv/ProfileController.java
+++ src/main/java/com/vantiv/ProfileController.java
@@ -1,17 +1,39 @@
 package com.vantiv;
 
-import java.util.Map;
 
+import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.facebook.api.Facebook;
+import org.springframework.social.facebook.api.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/profile")
 public class ProfileController {
 
-    @RequestMapping("/profile")
-    public String profile(Map<String, Object> model) {
-    	model.put("username", "Rich Hill");
-        return "profile";
+    private Facebook facebook;
+    private ConnectionRepository connectionRepository;
+
+    public ProfileController(Facebook facebook, ConnectionRepository connectionRepository) {
+        this.facebook = facebook;
+        this.connectionRepository = connectionRepository;
     }
 
-}
+    @GetMapping
+    public String helloFacebook(Model model) {
+        if (connectionRepository.findPrimaryConnection(Facebook.class) == null) {
+            return "redirect:/connect/facebook";
+        }
+
+        String [] fields = { "id", "email",  "name" };
+        User userProfile = facebook.fetchObject("me", User.class, fields);
+                
+        model.addAttribute("userName", userProfile.getName());
+        model.addAttribute("email", userProfile.getName());
+        model.addAttribute("userId", userProfile.getId());
+        return "profile";
+    }
+    
+ }
