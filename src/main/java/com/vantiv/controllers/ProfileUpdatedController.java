6865a4fd7a2aabb7a6cb262dfde7ce7c332336cc
Gary T. Usleaman <gusleaman@gmail.com>
2017-04-23 14:33:21 -0400

    navbar correction

--- src/main/java/com/vantiv/controllers/ProfileUpdatedController.java
+++ src/main/java/com/vantiv/controllers/ProfileUpdatedController.java
@@ -11,12 +11,12 @@ import com.vantiv.model.TixUser;
 import com.vantiv.services.TixUserService;
 
 @Controller
-public class UpdateProfileController {
+public class ProfileUpdatedController {
 
 	@Autowired
 	TixUserService tixUserService;
 
-	@PostMapping(value="/updateProfile")
+	@PostMapping(value="/profileUpdated")
 	public String updateProfile(HttpServletRequest request, @ModelAttribute TixUser profile) {
 		System.out.println("Getting form variables");
 		Long userId = 111709336056896L;
